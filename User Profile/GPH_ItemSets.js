908aec67d02b36208ac01190b253b13ec6fc7f05
NoDeath <liinuz@live.se>
2023-01-30 19:38:37 +0100

    Initialziled GPH_ItemSets Script

---
+++ User Profile/GPH_ItemSets.js
@@ -0,0 +1,14 @@
+// ==UserScript==
+// @name         GazelleGames GPH Item Set Info
+// @version      0.0.1
+// @description  Calculates which item set to use by the amount of GPH you currently have.
+// @author       Piitchyy
+// @match        https://*.gazellegames.net/user.php?id=*
+// @grant        none
+// @license      MIT
+// ==/UserScript==
+
+// Just setting up for coming code
+(function () {
+  'use strict';
+})();

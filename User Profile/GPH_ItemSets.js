e254ab8727131fe34b5a8ac288a5e98b762b30cf
NoDeath <liinuz@live.se>
2023-02-07 23:08:05 +0100

    It now uses fetch instead of xmlhttprequest

--- User Profile/GPH_ItemSets.js
+++ User Profile/GPH_ItemSets.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name         GazelleGames GPH Item Set Info
-// @version      0.2.1
+// @version      0.2.2
 // @description  Calculates which item set to use by the amount of GPH you currently have.
 // @author       Piitchyy
 // @namespace    https://github.com/No-Death/GGn-Scripts
@@ -9,7 +9,6 @@
 // @grant        GM.getValue
 // @grant        GM.setValue
 // @grant        GM.deleteValue
-// @grant        GM_xmlhttpRequest
 // @run-at       document-end
 // ==/UserScript==
 
@@ -34,52 +33,53 @@
   }
 
   // Start the API request
-  GM_xmlhttpRequest({
+  const endpoint = `https://gazellegames.net/api.php?request=user&id=${myUserID}`;
+  const options = {
     method: 'GET',
-    url: `https://gazellegames.net/api.php?request=user&id=${myUserID}`,
+    mode: 'same-origin',
+    credentials: 'omit',
+    redirect: 'error',
+    referrerPolicy: 'no-referrer',
     headers: {
       'X-API-Key': apiKey,
     },
-
-    onload: async function (response) {
-      // If the API returns 401, delete API key
-      if (response.status === 401) {
-        await GM.deleteValue('apiKey');
-        alert(
-          `You entered the wrong API key for 'GazelleGames GPH Item Set Info' \nMake sure it uses the "USER" permissions.`
-        );
-        location.reload();
-      }
-      // API was correct, continue.
-      const data = JSON.parse(response.responseText);
-      const gph = data.response.community.hourlyGold;
-      const buff = data.response.buffs.TorrentsGold;
-      const baseGph = gph / buff;
-      const sidebar = document.getElementById('items_navigation');
-      let newhtml = '';
-      sidebar.innerHTML += '<br><h3>GPH Information</h3>';
-      newhtml += `<tr><td>Base GPH:</td> <td>${Math.trunc(baseGph)}</td></tr>`;
-      newhtml += `<tr><td>Buff GPH:</td> <td>${Math.trunc(gph)}</td></tr>`;
-      if (baseGph >= 980) {
-        newhtml += `<tr><td>Set to use:</td> <td><a href="https://gazellegames.net/shop.php?search=empowered%20amethyst%20fortune&item_type=%5B%5D&cost_type=all&cost_amount=&order_by=dateadded&order_way=desc&category=All">Amethyst Set!</a></td></tr>`;
-      } else if (baseGph >= 180) {
-        newhtml += `<tr><td>Set to use:</td> <td><a href="https://gazellegames.net/shop.php?search=empowered%20jade%20fortune&item_type=%5B%5D&cost_type=all&cost_amount=&order_by=dateadded&order_way=desc&category=All">Jade Set!</a></td></tr>`;
-      } else if (baseGph >= 20) {
-        newhtml += `<tr><td>Set to use:</td> <td><a href="https://gazellegames.net/shop.php?search=empowered%20quartz%20fortune&item_type=%5B%5D&cost_type=all&cost_amount=&order_by=dateadded&order_way=desc&category=All">Quartz Set!</a></td></tr>`;
-      } else {
-        newhtml += `<tr><td>Set to use:</td> <td>No set needed!</td></tr>`;
-      }
-      newhtml += `<tr><td><a href="https://gazellegames.net/shop.php?ItemID=2582">Baguette?:</a></td> <td>${
-        baseGph >= 300 ? 'Yes' : 'No'
-      }</td></tr>`;
-
-      sidebar.innerHTML +=
-        `<table id="ggn_gph_information">` + newhtml + `</table>`;
-    },
-    onerror: function (response) {
-      console.log('Something went wrong');
-    },
-  });
+  };
+  const response = await fetch(endpoint, options);
+  // If the API returns 401, delete API key
+  if (response.status === 401) {
+    await GM.deleteValue('apiKey');
+    alert(
+      `You entered the wrong API key for 'GazelleGames GPH Item Set Info' \nMake sure it uses the "USER" permissions.`
+    );
+    // API was correct, continue.
+  } else if (response.status === 200) {
+    const data = await response.json();
+    const gph = data.response.community.hourlyGold;
+    const buff = data.response.buffs.TorrentsGold;
+    const baseGph = gph / buff;
+    const sidebar = document.getElementById('items_navigation');
+    let newhtml = '';
+    sidebar.innerHTML += '<br><h3>GPH Information</h3>';
+    newhtml += `<tr><td>Base GPH:</td> <td>${Math.trunc(baseGph)}</td></tr>`;
+    newhtml += `<tr><td>Buff GPH:</td> <td>${Math.trunc(gph)}</td></tr>`;
+    if (baseGph >= 980) {
+      newhtml += `<tr><td>Set to use:</td> <td><a href="https://gazellegames.net/shop.php?search=empowered%20amethyst%20fortune&item_type=%5B%5D&cost_type=all&cost_amount=&order_by=dateadded&order_way=desc&category=All">Amethyst Set!</a></td></tr>`;
+    } else if (baseGph >= 180) {
+      newhtml += `<tr><td>Set to use:</td> <td><a href="https://gazellegames.net/shop.php?search=empowered%20jade%20fortune&item_type=%5B%5D&cost_type=all&cost_amount=&order_by=dateadded&order_way=desc&category=All">Jade Set!</a></td></tr>`;
+    } else if (baseGph >= 20) {
+      newhtml += `<tr><td>Set to use:</td> <td><a href="https://gazellegames.net/shop.php?search=empowered%20quartz%20fortune&item_type=%5B%5D&cost_type=all&cost_amount=&order_by=dateadded&order_way=desc&category=All">Quartz Set!</a></td></tr>`;
+    } else {
+      newhtml += `<tr><td>Set to use:</td> <td>No set needed!</td></tr>`;
+    }
+    newhtml += `<tr><td><a href="https://gazellegames.net/shop.php?ItemID=2582">Baguette?:</a></td> <td>${
+      baseGph >= 300 ? 'Yes' : 'No'
+    }</td></tr>`;
+    sidebar.innerHTML +=
+      `<table id="ggn_gph_information">` + newhtml + `</table>`;
+    //Something went REALLY wrong
+  } else {
+    console.log('Something went wrong');
+  }
   // Set the time when the request was made
   await GM.setValue('lastFetch', Date.now());
 })();

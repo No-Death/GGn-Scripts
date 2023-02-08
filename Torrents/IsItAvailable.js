b3c800f0cd25f2703b8216f865c7eaba84ed580d
NoDeath <liinuz@live.se>
2023-02-08 17:00:46 +0100

    Init IsItAvailable

---
+++ Torrents/IsItAvailable.js
@@ -0,0 +1,16 @@
+// ==UserScript==
+// @name         GazelleGames Is it Available?
+// @namespace    https://github.com/No-Death/GGn-Scripts
+// @version      0.0.1
+// @description  Will check if the current game you are browsing on the steamstore is available on GGn
+// @author       Piitchyy
+// @match        *://store.steampowered.com/app/*
+// @license      MIT
+// @grant        GM.getValue
+// @grant        GM.setValue
+// @grant        GM.deleteValue
+// ==/UserScript==
+
+(async function () {
+  'use strict';
+})();
